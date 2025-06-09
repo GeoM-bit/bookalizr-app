d0126fa0a87d1bb252951d6e2e7b48a0d5d3d0d0
Georgiana Matei <54259870+GeoM-bit@users.noreply.github.com>
2025-06-09 17:06:01 +0300

    Update firebaseConfig.jsx

--- app/firebaseConfig.jsx
+++ app/firebaseConfig.jsx
@@ -2,12 +2,7 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDr3WQrsicXaB75PxzZLDX7PYi9OWEAU9w",
-  authDomain: "bookalizr-d7391.firebaseapp.com",
-  projectId: "bookalizr-d7391",
-  storageBucket: "bookalizr-d7391.firebasestorage.app",
-  messagingSenderId: "616798399540",
-  appId: "1:616798399540:web:c5201c03ab7bace5ff6e61"
+
 };
 
 const app = initializeApp(firebaseConfig);
